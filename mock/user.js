ec42f5d36d96d9671796a46d51e15efa7c922c32
BlueStar2333 <1061368119@qq.com>
2024-08-14 10:49:05 +0800

    1

---
+++ mock/user.js
@@ -0,0 +1,118 @@
+
+const tokens = {
+  admin: {
+    token: 'admin-token'
+  },
+  editor: {
+    token: 'editor-token'
+  }
+}
+
+const users = {
+  'admin-token': {
+    roles: ['admin'],
+    introduction: 'I am a super administrator',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Super Admin'
+  },
+  'editor-token': {
+    roles: ['editor'],
+    introduction: 'I am an editor',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Normal Editor'
+  }
+}
+
+const UnitList={
+  'data':[{
+    'code':'B',
+    'unit':'杯',
+  },{
+    'code':'D',
+    'unit':'袋',
+  }]
+    //   [{
+    //     code: 'B',
+    //     unit: '杯',
+    // }, {
+    //     code: 'D',
+    //     unit: '袋',
+    // },{
+    //     code: 'C',
+    //     unit: '可'
+    // },{
+    //     code: 'H',
+    //     unit: 'x'
+    // }]
+
+}
+module.exports = [
+  // user login
+  {
+    url: '/reagentcabinetmanagement/user/login',
+    type: 'post',
+    response: config => {
+      const { username } = config.body
+      const token = tokens[username]
+
+      // mock error
+      if (!token) {
+        return {
+          code: 60204,
+          message: 'Account and password are incorrect.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: token
+      }
+    }
+  },
+
+  // get user info
+  {
+    url: '/reagentcabinetmanagement/user/info\.*',
+    type: 'get',
+    response: config => {
+      const { token } = config.query
+      const info = users[token]
+
+      // mock error
+      if (!info) {
+        return {
+          code: 50008,
+          message: 'Login failed, unable to get user details.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: info
+      }
+    }
+  },
+
+  // user logout
+  {
+    url: '/reagentcabinetmanagement/user/logout',
+    type: 'post',
+    response: _ => {
+      return {
+        code: 20000,
+        data: 'success'
+      }
+    }
+  },
+
+  {
+    url:'/reagentcabinetmanagement/UnitList',
+    type:'get',
+    response: config=>{
+      return{
+        code:20000,
+        data:UnitList
+      }
+    }
+  }
+]
